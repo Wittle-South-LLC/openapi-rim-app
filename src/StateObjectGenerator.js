8210dc4ca6c13afc2740f17b067fcb018955e877
Eric Wittle <eric@wittle.net>
2019-06-15 13:09:24 -0400

    #13 - Internationalization messages

--- src/StateObjectGenerator.js
+++ src/StateObjectGenerator.js
@@ -7,5 +7,29 @@ export default class StateObjectGenerator extends BaseModelGenerator {
       config['paths']['templates'] + '/' + config['templates']['stateObjects'],
       config['paths']['stateObjects'] + '/' + modelObject._name + '.js',
       false)
+    this._sections = ['intlMessage']
+  }
+
+  getFieldIntlMessages(prop) {
+    return [
+      `${prop.getIntlName()}Label: { id: '${prop.schemaName}.${prop.getIntlName()}', defaultMessage: '${prop.getMixedName()}' },`,
+      `${prop.getIntlName()}Placeholder: { id: '${prop.schemaName}.${prop.getIntlName()}', defaultMessage: '${prop.getMixedName()}...' },`,
+      `${prop.getIntlName()}Invalid: { id: '${prop.schemaName}.${prop.getIntlName()}', defaultMessage: '${prop.getMixedName()} is invalid' },`
+    ]    
+  }
+
+  // Initializes context with an empty array for each section
+  getInitialContext() {
+    const result = super.getInitialContext()
+    this._sections.forEach((key) => {
+      result[key] = []
+    })
+    return result
+  }
+
+  processProperty(context, prop) {
+    if (!prop.isId) {
+      context['intlMessage'] = context['intlMessage'].concat(this.getFieldIntlMessages(prop))
+    }
   }
 }
--- templates/StateObject.mu
+++ templates/StateObject.mu
@@ -1,8 +1,15 @@
 /* {{ name }}.js - State class for {{ name }} model objects */
+import { defineMessages } from 'react-intl'
 import Orim{{ name }} from './orim/Orim{{ name }}'
 
 export default class {{ name }} extends Orim{{name}} {
   constructor(createFrom, dirtyVal = false, fetchingVal = false, newVal = false) {
     super (createFrom, dirtyVal, fetchingVal, newVal)
   }
+
+  static msgs = defineMessages({
+    {{#each intlMessage }}
+    {{{ this }}}
+    {{/each}}  
+  })
 }
